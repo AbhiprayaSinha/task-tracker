48057f380059fd6bcd4cc9492454ac482228d2ac
Abhipraya Sinha <abhiprayasinha@gmail.com>
2024-02-02 17:47:59 +0530

    added frontend

--- task-tracker-backend/src/main/java/com/example/taskTrackerBackend/entity/Task.java
+++ task-tracker-backend/src/main/java/com/example/taskTrackerBackend/entity/Task.java
@@ -1,33 +1,36 @@
 package com.example.taskTrackerBackend.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDate;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "tasks")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Task {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String email;
+
+    @Column(name="title", nullable = false)
+    private String title;
+
+    @Column(name="description", nullable = false)
     private String description;
-    private LocalDate dueDate;
+
+    @Column(name="due_date", nullable = false)
+    private String due_date;
+
+    @Column(name="completed", nullable = false)
     private boolean completed;
-
-
-
-
-
 }
--- task-tracker-backend/src/main/java/com/example/taskTrackerBackend/repository/UserRepository.java
+++ task-tracker-backend/src/main/java/com/example/taskTrackerBackend/repository/UserRepository.java
@@ -1,4 +1,19 @@
 package com.example.taskTrackerBackend.repository;
 
-public class UserRepository {
+import java.util.Optional;
+
+import com.example.taskTrackerBackend.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByName(String username);
+
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByNameOrEmail(String username, String email);
+
+    Boolean existsByName(String username);
 }
