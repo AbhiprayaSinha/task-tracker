cca5fbefbf8a80c06cc95bc1f21af5d681a43716
Abhipraya Sinha <abhiprayasinha@gmail.com>
2024-02-02 14:59:55 +0530

    Implemented Exception and TaskRepository

--- task-tracker-backend/src/main/java/com/example/taskTrackerBackend/exception/Exception.java
+++ task-tracker-backend/src/main/java/com/example/taskTrackerBackend/exception/Exception.java
@@ -1,4 +1,14 @@
 package com.example.taskTrackerBackend.exception;
 
-public class Exception {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Exception extends RuntimeException {
+    private HttpStatus status;
+    private String message;
 }
--- task-tracker-backend/src/main/java/com/example/taskTrackerBackend/repository/TaskRepository.java
+++ task-tracker-backend/src/main/java/com/example/taskTrackerBackend/repository/TaskRepository.java
@@ -1,7 +1,13 @@
 package com.example.taskTrackerBackend.repository;
 
+import java.util.Optional;
+import java.util.Set;
+
 import com.example.taskTrackerBackend.entity.Task;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Long>{
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findByTitle(String title);
+    Set<Task> findByCompleted(boolean completed);
 }
